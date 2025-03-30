7755450f79b0d7aafc3d6cb4900dbd66fd85b258
ArthurEstevaum <arthurestevao631@gmail.com>
2025-03-30 13:22:26 -0300

    Refactor Contract controller

--- src/main/java/com/estevaum/car_rent_app/controllers/ContractController.java
+++ src/main/java/com/estevaum/car_rent_app/controllers/ContractController.java
@@ -1,19 +1,9 @@
 package com.estevaum.car_rent_app.controllers;
 
-import com.estevaum.car_rent_app.DTO.Contracts.CarContractInfoDTO;
 import com.estevaum.car_rent_app.DTO.Contracts.ContractInfoDTO;
-import com.estevaum.car_rent_app.DTO.Contracts.ContractUserInfoDTO;
 import com.estevaum.car_rent_app.DTO.Contracts.CreateContractDTO;
-import com.estevaum.car_rent_app.entities.Car;
 import com.estevaum.car_rent_app.entities.RentingContract;
-import com.estevaum.car_rent_app.entities.User;
-import com.estevaum.car_rent_app.enums.UserTypes;
-import com.estevaum.car_rent_app.exceptions.CarNotFoundException;
-import com.estevaum.car_rent_app.exceptions.NumberOfContractsExceeded;
-import com.estevaum.car_rent_app.exceptions.UserNotFoundException;
-import com.estevaum.car_rent_app.repositories.CarRepository;
-import com.estevaum.car_rent_app.repositories.RentingContractRepository;
-import com.estevaum.car_rent_app.repositories.UserRepository;
+import com.estevaum.car_rent_app.services.ContractService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,92 +17,27 @@ import java.util.List;
 public class ContractController {
 
     @Autowired
-    private RentingContractRepository contractRepository;
-
-    @Autowired
-    private CarRepository carRepository;
-
-    @Autowired
-    private UserRepository userRepository;
+    private ContractService contractService;
 
     @GetMapping("/")
     public ResponseEntity<List<ContractInfoDTO>> listAllContracts() {
-        List<RentingContract> contracts = contractRepository.findAll();
-
-        List<ContractInfoDTO> contractResponse = contracts.stream().map(contract -> {
-            return new ContractInfoDTO(contract.getId(), contract.getStartDate(), contract.getEndDate(),
-                    contract.getContractTotalPrice(), contract.isCurrentContract() , new CarContractInfoDTO(contract.getCar().getLicensePlate(),
-                    contract.getCar().getId(), contract.getCar().getModel().getManufacturer(),
-                    contract.getCar().getModel().getName(), contract.getCar().getModel().getCategory(),
-                    contract.getCar().getModel().getModelYear()), new ContractUserInfoDTO(contract.getUser().getId(),
-                    contract.getUser().getUsername(), contract.getUser().getEmail(), contract.getUser().getPhoneNumber(),
-                    contract.getUser().getUserType()));
-        }).toList();
-
-        return ResponseEntity.ok(contractResponse);
+        return ResponseEntity.ok(contractService.getAllContracts());
     }
 
     @GetMapping("/user/{username}")
     public ResponseEntity<List<ContractInfoDTO>> listUserContracts(@PathVariable String username) {
-        if(!userRepository.existsByUsername(username)) {
-            throw new UserNotFoundException();
-        }
-
-        List<RentingContract> userContracts = contractRepository.findByUserUsername(username);
-
-        List<ContractInfoDTO> contractResponse = userContracts.stream().map(contract -> {
-            return new ContractInfoDTO(contract.getId(), contract.getStartDate(), contract.getEndDate(),
-                    contract.getContractTotalPrice(), contract.isCurrentContract() , new CarContractInfoDTO(contract.getCar().getLicensePlate(),
-                    contract.getCar().getId(), contract.getCar().getModel().getManufacturer(),
-                    contract.getCar().getModel().getName(), contract.getCar().getModel().getCategory(),
-                    contract.getCar().getModel().getModelYear()), new ContractUserInfoDTO(contract.getUser().getId(),
-                    contract.getUser().getUsername(), contract.getUser().getEmail(), contract.getUser().getPhoneNumber(),
-                    contract.getUser().getUserType()));
-        }).toList();
-
-        return ResponseEntity.ok(contractResponse);
+        return ResponseEntity.ok(contractService.getUserContracts(username));
     };
 
     @GetMapping("/car/{id}")
     public ResponseEntity<List<ContractInfoDTO>> listCarContracts(@PathVariable Long id) {
-        if(!carRepository.existsById(id)) {
-            throw new CarNotFoundException("Carro não encontrado");
-        }
-
-        List<RentingContract> carContracts = contractRepository.findByCarId(id);
-
-        List<ContractInfoDTO> contractResponse = carContracts.stream().map(contract -> {
-            return new ContractInfoDTO(contract.getId(), contract.getStartDate(), contract.getEndDate(),
-                    contract.getContractTotalPrice(), contract.isCurrentContract() , new CarContractInfoDTO(contract.getCar().getLicensePlate(),
-                    contract.getCar().getId(), contract.getCar().getModel().getManufacturer(),
-                    contract.getCar().getModel().getName(), contract.getCar().getModel().getCategory(),
-                    contract.getCar().getModel().getModelYear()), new ContractUserInfoDTO(contract.getUser().getId(),
-                    contract.getUser().getUsername(), contract.getUser().getEmail(), contract.getUser().getPhoneNumber(),
-                    contract.getUser().getUserType()));
-        }).toList();
-
-        return ResponseEntity.ok(contractResponse);
+        return ResponseEntity.ok(contractService.getCarContracts(id));
     }
 
     @PostMapping("/")
     public ResponseEntity<String> createRentingContract(@RequestBody @Valid CreateContractDTO requestData) {
-        User user = userRepository.findByUsername(requestData.username()).orElseThrow(UserNotFoundException::new);
+        RentingContract contract = contractService.createRentingContract(requestData);
 
-        if(user.hasActiveContracts() && user.getUserType() == UserTypes.personal) {
-            throw new NumberOfContractsExceeded("Uma pessoa física pode ter apenas um contrato ativo.");
-        }
-
-        Car car = carRepository.findById(requestData.carId()).orElseThrow(() -> new CarNotFoundException("Carro não encontrado."));
-
-        Boolean carHasActiveContracts = contractRepository.existsByCarId(car.getId());
-
-        if(carHasActiveContracts) {
-            throw new NumberOfContractsExceeded("Um carro pode ter apenas um contrato ativo.");
-        }
-
-        RentingContract newContract = new RentingContract(car, requestData.endDate(), requestData.startDate(), user);
-        contractRepository.save(newContract);
-
-        return ResponseEntity.ok("Contrato efetuado com sucesso.");
+        return ResponseEntity.ok("Contrato de nº " + contract.getId() + " efetuado com sucesso.");
     }
 }
---
+++ src/main/java/com/estevaum/car_rent_app/services/ContractService.java
@@ -0,0 +1,87 @@
+package com.estevaum.car_rent_app.services;
+
+import com.estevaum.car_rent_app.DTO.Contracts.CarContractInfoDTO;
+import com.estevaum.car_rent_app.DTO.Contracts.ContractInfoDTO;
+import com.estevaum.car_rent_app.DTO.Contracts.ContractUserInfoDTO;
+import com.estevaum.car_rent_app.DTO.Contracts.CreateContractDTO;
+import com.estevaum.car_rent_app.entities.Car;
+import com.estevaum.car_rent_app.entities.RentingContract;
+import com.estevaum.car_rent_app.entities.User;
+import com.estevaum.car_rent_app.enums.UserTypes;
+import com.estevaum.car_rent_app.exceptions.CarNotFoundException;
+import com.estevaum.car_rent_app.exceptions.NumberOfContractsExceeded;
+import com.estevaum.car_rent_app.exceptions.UserNotFoundException;
+import com.estevaum.car_rent_app.repositories.CarRepository;
+import com.estevaum.car_rent_app.repositories.RentingContractRepository;
+import com.estevaum.car_rent_app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class ContractService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Autowired
+    private RentingContractRepository contractRepository;
+
+    private final Function<RentingContract, ContractInfoDTO> castContractToInfo =  contract -> {
+        return new ContractInfoDTO(contract.getId(), contract.getStartDate(), contract.getEndDate(),
+                contract.getContractTotalPrice(), contract.isCurrentContract() , new CarContractInfoDTO(contract.getCar().getLicensePlate(),
+                contract.getCar().getId(), contract.getCar().getModel().getManufacturer(),
+                contract.getCar().getModel().getName(), contract.getCar().getModel().getCategory(),
+                contract.getCar().getModel().getModelYear()), new ContractUserInfoDTO(contract.getUser().getId(),
+                contract.getUser().getUsername(), contract.getUser().getEmail(), contract.getUser().getPhoneNumber(),
+                contract.getUser().getUserType()));
+    };
+
+    public List<ContractInfoDTO> getAllContracts() {
+        List<RentingContract> contracts = contractRepository.findAll();
+
+        return contracts.stream().map(castContractToInfo).toList();
+    }
+
+    public List<ContractInfoDTO> getUserContracts(String username) {
+        if(!userRepository.existsByUsername(username)) {
+            throw new UserNotFoundException();
+        }
+
+        List<RentingContract> userContracts = contractRepository.findByUserUsername(username);
+
+        return userContracts.stream().map(castContractToInfo).toList();
+    }
+
+    public List<ContractInfoDTO> getCarContracts(Long id) {
+        if(!carRepository.existsById(id)) {
+            throw new CarNotFoundException("Carro não encontrado");
+        }
+
+        List<RentingContract> carContracts = contractRepository.findByCarId(id);
+
+        return carContracts.stream().map(castContractToInfo).toList();
+    }
+
+    public RentingContract createRentingContract(CreateContractDTO requestData) {
+        User user = userRepository.findByUsername(requestData.username()).orElseThrow(UserNotFoundException::new);
+
+        if(user.hasActiveContracts() && user.getUserType() == UserTypes.personal) {
+            throw new NumberOfContractsExceeded("Uma pessoa física pode ter apenas um contrato ativo.");
+        }
+
+        Car car = carRepository.findById(requestData.carId()).orElseThrow(() -> new CarNotFoundException("Carro não encontrado."));
+
+        Boolean carHasActiveContracts = contractRepository.existsByCarId(car.getId());
+
+        if(carHasActiveContracts) {
+            throw new NumberOfContractsExceeded("Um carro pode ter apenas um contrato ativo.");
+        }
+
+        RentingContract newContract = new RentingContract(car, requestData.endDate(), requestData.startDate(), user);
+        return contractRepository.save(newContract);
+    }
+}
